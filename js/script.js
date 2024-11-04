16ecdac713daf1262ff2e3f8e87f5758e09493b5
Ramen Biswas <133987263+devsramen@users.noreply.github.com>
2024-11-04 09:17:11 +0600

    Wrong Calculation BUG solved at 04-11-2024_0917

--- js/script.js
+++ js/script.js
@@ -3,32 +3,33 @@ var button = document.querySelector("button");
 
 
 button.addEventListener("click",function(){
-    if(input.value <=0){
+    let age = Number(input.value);
+    if(age <=0){
         console.log("Age can not be Zero or Negative value")
         document.write(`<h1>Age can not be Zero or Negative value</h1>`)
         document.write(`<a href="./start.html" style="background-color: green; color: white; font-weight: 600; border-radius: 5px; padding: 15px; text-decoration: none; box-sizing: border-box; display: inline-block">GO Back</a>`)
     }
-    else if(input.value > 0 && input.value <= 1){
+    else if(age > 0 && age <= 1){
         console.log("You are Infant") // Infants: 0 to below 1 year
         document.write(`<h1>You are Infant</h1>`)
         document.write(`<a href="./start.html" style="background-color: green; color: white; font-weight: 600; border-radius: 5px; padding: 15px; text-decoration: none; box-sizing: border-box; display: inline-block">GO Back</a>`)
     }
-    else if(input.value > 1 && input.value <= 3){
+    else if(age > 1 && age <= 3){
         console.log("You are Toddler") // Toddlers: 1-3 years
         document.write(`<h1>You are Toddler</h1>`)
         document.write(`<a href="./start.html" style="background-color: green; color: white; font-weight: 600; border-radius: 5px; padding: 15px; text-decoration: none; box-sizing: border-box; display: inline-block">GO Back</a>`)
     }
-    else if(input.value > 3 && input.value <= 5){
+    else if(age > 3 && age <= 5){
         console.log("You are Preschooler") // Preschoolers: 4-5 years
         document.write(`<h1>You are Preschooler</h1>`)
         document.write(`<a href="./start.html" style="background-color: green; color: white; font-weight: 600; border-radius: 5px; padding: 15px; text-decoration: none; box-sizing: border-box; display: inline-block">GO Back</a>`)
     }
-    else if(input.value > 5 && input.value < 13){
+    else if(age > 5 && age < 13){
         console.log("You are Child") // School-age children: 6 below 13 years
         document.write(`<h1>You are Child</h1>`)
         document.write(`<a href="./start.html" style="background-color: green; color: white; font-weight: 600; border-radius: 5px; padding: 15px; text-decoration: none; box-sizing: border-box; display: inline-block">GO Back</a>`)
     }
-    else if(input.value >= 13 && input.value < 18){
+    else if(age >= 13 && age < 18){
         console.log("You are Teenage") // teenagers are 13 below 18
         document.write(`<h1>You are Teenage</h1>`)
         document.write(`<a href="./start.html" style="background-color: green; color: white; font-weight: 600; border-radius: 5px; padding: 15px; text-decoration: none; box-sizing: border-box; display: inline-block">GO Back</a>`)
@@ -52,7 +53,7 @@ var rangeBtn = document.getElementById("rangeBtn");
 
 // For Single Button
 singleBtn.addEventListener("click", function(){
-    var number = inputSingle.value;
+    var number = Number(inputSingle.value);
     document.write(`<b>Multiplication Table for ${number}</b>` + "<br>")
     for(i=1; i<=10;i++){
         document.write(number + " x " + i + " = " + number*i + "<br>")
@@ -62,8 +63,8 @@ singleBtn.addEventListener("click", function(){
 
 // For Multiple Button
 rangeBtn.addEventListener("click", function(){
-    var startValue = inputRangeForm.value;
-    var endValue = inputRangeTo.value;
+    var startValue = Number(inputRangeForm.value);
+    var endValue = Number(inputRangeTo.value);
     document.write(`<a href="./start.html" style="background-color: green; color: white; font-weight: 600; border-radius: 5px; padding: 15px; text-decoration: none; box-sizing: border-box; ">GO Back</a> <br><br>`)
     for(k=startValue; k<=endValue;k++){        
         document.write(`<b>Multiplication Table for ${k}</b>` + "<br>")
